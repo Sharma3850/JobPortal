e408249bef6dc1107611e3282e5128318741670c
Sharma3850 <yogesh.sharma3850@gmail.com>
2024-07-18 19:30:22 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'perfectJob',
+  webDir: 'www'
+};
+
+export default config;
