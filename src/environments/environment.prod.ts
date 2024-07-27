4e57e0d806df487027e8097d4ecbfc416e7e89ee
Yogesh <72644648+Sharma3850@users.noreply.github.com>
2024-07-27 19:08:31 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,12 +4,12 @@ export const environment = {
   
   
   firebaseConfig: {
-    apiKey: "AIzaSyB3LmeKCPqui715rry0tkNh1xQWUpXC_RM",
-    authDomain: "jobportal-893ca.firebaseapp.com",
-    projectId: "jobportal-893ca",
-    storageBucket: "jobportal-893ca.appspot.com",
-    messagingSenderId: "1057054666276",
-    appId: "1:1057054666276:web:a83e5a35bcb3aacc9bb2b1",
-    measurementId: "G-HE7N8JW2FR"
+    apiKey: "Enter your api key",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   },
 };
