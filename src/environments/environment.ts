e8a8fc465a69a17c024d8776af5a6e5549614a83
Yogesh <72644648+Sharma3850@users.noreply.github.com>
2024-07-27 19:09:02 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,19 +2,21 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+
 export const environment = {
   production: false,
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  
   firebaseConfig: {
-    apiKey: "AIzaSyB3LmeKCPqui715rry0tkNh1xQWUpXC_RM",
-    authDomain: "jobportal-893ca.firebaseapp.com",
-    projectId: "jobportal-893ca",
-    storageBucket: "jobportal-893ca.appspot.com",
-    messagingSenderId: "1057054666276",
-    appId: "1:1057054666276:web:a83e5a35bcb3aacc9bb2b1",
-    measurementId: "G-HE7N8JW2FR"
+    apiKey: "Enter your api key",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   },
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
